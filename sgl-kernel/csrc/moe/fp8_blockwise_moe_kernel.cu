24866017bba36b8d240e2ecb9ba765af24974010
Johnny <johnnynuca14@gmail.com>
2026-01-15 00:52:32 +0100

    Change MmaTileShape from 128 to 64

--- sgl-kernel/csrc/moe/fp8_blockwise_moe_kernel.cu
+++ sgl-kernel/csrc/moe/fp8_blockwise_moe_kernel.cu
@@ -485,7 +485,7 @@ void sm90_fp8_blockwise_group_mm_dispatch_shape(
 
   struct MmaConfigHx00AndH20SmallK {
     using ElementA = cutlass::float_e4m3_t;
-    using MmaTileShape = Shape<_128, _128, _128>;
+    using MmaTileShape = Shape<_64, _128, _128>;
     using ClusterShape = Shape<_1, _2, _1>;
     using KernelSchedule = cutlass::gemm::KernelPtrArrayTmaWarpSpecializedCooperativeFP8Blockwise;
     using EpilogueSchedule = cutlass::epilogue::PtrArrayTmaWarpSpecializedCooperative;
